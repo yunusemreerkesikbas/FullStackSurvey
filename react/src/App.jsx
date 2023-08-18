88917cf2c3850617cf0177dc4493c803de27fa7b
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2023-08-19 01:40:58 +0300

    initial package files

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+
+)
+}
+
+import './App.css'
+
+export default App
